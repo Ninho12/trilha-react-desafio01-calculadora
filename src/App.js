5e2fd1bc854726ac49100a1f5905e7413fa9a418
João Paulo <joaopaulojpsp@gmail.com>
2022-11-28 09:37:05 -0300

    Adição das funcoes multiplicação e divição

--- src/App.js
+++ src/App.js
@@ -49,6 +49,34 @@ const App = () => {
 
   }
 
+  const handleMultNumbers = () => {
+
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('X')
+    }else {
+      const num = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(num))
+      setOperation('')
+    }
+
+  }
+
+  const handleDivNumbers = () => {
+
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('/')
+    }else {
+      const num = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(num))
+      setOperation('')
+    }
+
+  }
+
   const handleEquals = () => {
 
     if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
@@ -59,6 +87,12 @@ const App = () => {
           case '-':
             handleMinusNumbers();
             break;
+            case 'X':
+              handleMultNumbers();
+              break; 
+            case '/':
+              handleDivNumbers();
+              break;  
           default: 
             break;
         }
@@ -71,8 +105,8 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x"/>
-          <Button label="/"/>
+          <Button label="X" onClick={handleMultNumbers}/>
+          <Button label="/" onClick={handleDivNumbers}/>
           <Button label="c" onClick={handleOnClear}/>
           <Button label="."/>
         </Row>
